250fd243bce53bd31707522b5e1eb37e80cbe8a5
MartinPC <MartinChrisPC@hotmail.com>
2024-11-27 23:01:26 -0500

    feat: update CartRepository

--- Backend/src/main/java/com/martin/api/persistence/repository/CartRepository.java
+++ Backend/src/main/java/com/martin/api/persistence/repository/CartRepository.java
@@ -1,8 +1,10 @@
 package com.martin.api.persistence.repository;
 
 import com.martin.api.persistence.model.Cart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+  Optional<Cart> findByUserId(Long userId);
 }
